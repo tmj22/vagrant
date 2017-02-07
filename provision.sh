ca5e1c8c36980fbfd4fe512d82f1f4a3345fe3e9
tmj22 <tom_mj91@hotmail.com>
2017-02-07 19:09:57 +0100

    3c

---
+++ provision.sh
@@ -0,0 +1,28 @@
+sudo apt-get -y update
+sudo apt-get -y install apache2
+sudo apt-get -y install git
+
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+# Instalación de PHP
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+
+
+sudo rm -rf /var/www/sitio1
+sudo rm -rf /var/www/sitio2
+sudo rm -rf /var/www/index.html
+#clonar las paginas de ejemplo
+git clone https://github.com/Franpastoragusti/sitio1.git /var/www/sitio1
+git clone https://github.com/Franpastoragusti/sitio2.git /var/www/sitio2
+
+#movemos los archivos de configuracion al apache
+sudo mv /var/www/sitio1/sitio1.conf /etc/apache2/sites-available/sitio1.conf
+sudo mv /var/www/sitio2/sitio2.conf /etc/apache2/sites-available/sitio2.conf
+
+cd /etc/apache2/sites-available/
+sudo a2ensite sitio1.conf
+sudo a2ensite sitio2.conf
+sudo a2dissite default
+sudo service apache2 reload
